c685feeb52787318a59cbdc98a31e7b2a485a58d
mi <example@example.com>
2020-11-13 13:47:49 +0900

    updated_at,created_at無し

--- app/ConfirmUsers.php
+++ app/ConfirmUsers.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ConfirmUsers extends Model
 {
-            /**
+    public $timestamps = false;
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
--- app/Users.php
+++ app/Users.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 class Users extends Authenticatable
 {
     use Notifiable;
-
+    public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
