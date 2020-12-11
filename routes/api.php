7f58ed6fc6565750f89a1236aa4c244113d67a04
mi <example@example.com>
2020-12-11 12:59:13 +0900

    ルーティングにコメント付けた

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,44 +12,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::group(['prefix' => 'v1', 'namespace' => 'V1'], function () {
+// ユーザー
     Route::post('user', 'UserController@store');
     Route::patch('user/{id}', 'UserController@update');
     Route::delete('user/{id}', 'UserController@destroy');
-
+// グループ
     Route::get('group/{id}', 'GroupController@show');
     Route::post('group', 'GroupController@store');
     Route::patch('group/{id}', 'GroupController@update');
     Route::delete('group', 'GroupController@destroy');
-
+// グループメンバー
     Route::get('groupmember', 'GroupMemberController@index');
     Route::post('groupmember', 'GroupMemberController@store');
     Route::delete('groupmember/{id}', 'GroupMemberController@destroy');
-
+// シフト作成
     Route::get('makeshift', 'MakeShiftController@index');
     Route::post('makeshift', 'MakeShiftController@store');
     Route::patch('makeshift/{id}', 'MakeShiftController@update');
     Route::delete('makeshift', 'MakeShiftController@destroy');
-
+// シフト希望
+    Route::get('wishshift', 'WishShiftController@index');
+// 希望調査
     Route::get('wishsurvey', 'WishSurveyController@index');
     Route::get('wishsurvey/{id}', 'WishSurveyController@show');
     Route::patch('wishsurvey', 'WishSurveyController@update');
     Route::post('wishsurvey', 'WishSurveyController@store');
-
-    Route::get('operator', 'OperatorController@index');
-
-    Route::get('homesidemenu/{id}', 'HomeSideMenuController@show');
-
-    Route::get('groupsidemenu/{id}', 'GroupSideMenuController@show');
-
+// ホームカレンダー
     Route::get('Calendar/{id}', 'CalendarController@show');
-
+// 共有シフト
     Route::get('share/{id}', 'SharController@show');
-
-    Route::get('wishshift', 'WishShiftController@index');
-
+// ホームサイドメニュー
+    Route::get('homesidemenu/{id}', 'HomeSideMenuController@show');
+// グループサイドメニュー
+    Route::get('groupsidemenu/{id}', 'GroupSideMenuController@show');
+// 権限
     Route::get('authority', 'AuthorityController@index');
     Route::get('authority/{id}', 'AuthorityController@show');
     Route::patch('authority/{id}', 'AuthorityController@update');
     Route::post('authority', 'AuthorityController@store');
     Route::delete('authority/{id}', 'AuthorityController@destroy');
+// オペレーター権限
+    Route::get('operator', 'OperatorController@index');
 });
